f226c460309e77a3a7464cee8d512569665fcb07
Bohdan Frentser <bohdan.frentser@gmail.com>
2021-12-07 18:36:16 +0200

    Update task 02

--- js/task-02.js
+++ js/task-02.js
@@ -4,14 +4,12 @@ const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Con
 // Добавит название ингредиента как его текстовое содержимое.
 // Добавит элементу класс item.
 // После чего вставит все <li> за одну операцию в список ul.ingredients.
-// const ulIngredients = document.querySelector('#ingredients');
-// const elements = ingredients.map(option => {
-//   const liEl = document.createElement('li');
-//   liEl.textContent = option;
-//   liEl.classList.add('item');
-//   return liEl;
-// });
+const ulIngredients = document.querySelector('#ingredients');
+const elements = ingredients.map(option => {
+  const liEl = document.createElement('li');
+  liEl.textContent = option;
+  liEl.classList.add('item');
+  return liEl;
+});
 
-// console.log(elements);
-
-// ulIngredients.append(...elements);
+console.log(elements);
